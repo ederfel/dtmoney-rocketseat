8229f0afe693eb89081396cbb1fb9cc90fd76c06
Eder Feliciano <eder@mundowap.com.br>
2021-07-09 08:15:39 -0300

    First Commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+interface HeaderPropos{
+    onOpenNewTransactionModal: () => void;
+}
+export function Header({onOpenNewTransactionModal}: HeaderPropos) {
+
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <button type="button" onClick={onOpenNewTransactionModal} >
+                    Nova Transação
+                </button>
+            </Content>
+        </Container>
+    )
+}
